bc1d6aef3d5c89a5e5a3bc4e7a942ac7864a7fa7
blueslurpee <blueslurpee@blueslurpee.com>
2021-06-13 15:21:04 +0200

    order struct

--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import "fmt"
 import "github.com/blueslurpee/browserbook/queue"
+import "github.com/blueslurpee/browserbook/order"
 
 func main() {
   q := queue.InitQueue()
-  fmt.Println(q)
+
+  for i := 0; i < 10; i++ {
+    order := &order.Order{Action: 1, Type: 1, Price: 45.23, Quantity: 4.1}
+    q.Enqueue(order)
+  }
+
+  for q.IsEmpty() != true {
+    fmt.Println(q.Dequeue())
+  }
 }
