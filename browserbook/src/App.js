89f321156897ea89a0a431958a8357ab8bef2570
blueslurpee <blueslurpee@blueslurpee.com>
2021-06-27 21:11:02 +0200

    Rearrange

--- browserbook/src/App.js
+++ browserbook/src/App.js
@@ -1,43 +1,25 @@
-import React, { Component } from 'react';
-
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      message: '',
-      mod: null,
-      inst: null,
-    }
-  }
-
-  async componentDidMount() {
-    {/*// let { instance, module } = await WebAssembly.instantiateStreaming(fetch("main.wasm"), window.go.importObject)
-    // await window.go.run(instance)*/}
-    this.setState({
-      mod: module,
-    })
-  }
-
-  render() {
-    return (
-      <div className="App">
-      {/*
-        // <form>
-        //   <input type="text" name="" id="userInput" onChange={(e) => this.handleChange(e)} style={{ marginTop: '100px' }} />
-        //   <br />
-        //   <button type="submit" onClick={(e) => this.handleSubmit(e)}>Click me to see MAGIC!!</button>
-        // </form> */}
-        <br />
-        <span id="message">
-          Hello World
-        </span>
-      </div>
-    )
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
