8152f77fa5b82c712de422d6799f5ff02d6e8d3e
tejareddy8888 <saitejapottanigari@gmail.com>
2021-09-23 23:38:02 +0200

    FINALLY

--- client/src/components/OrderMatching.tsx
+++ client/src/components/OrderMatching.tsx
@@ -3,18 +3,36 @@ import React, { useEffect, useState } from "react";
 import "tailwindcss/tailwind.css";
 import { ethers, providers, Contract } from "ethers";
 import { useAppContext } from "./context/Store";
+import { SelectObject, Select } from "./elements/inputs/Select";
 import Info from "./elements/Info";
 import { filter, matches } from "lodash";
-import { token2Address, token2Id, EXCHANGE,TOKENONE, exchangeAbi,tokenOneAbi } from "../constants";
+import { Tokens } from "../types/Token";
+import {
+  token2Address,
+  token2Id,
+  EXCHANGE,
+  TOKENONE,
+  exchangeAbi,
+  tokenOneAbi,
+  token2Abi,
+} from "../constants";
 
 function OrderMatch() {
   const { state, setContext } = useAppContext();
+
+  const [token, setToken] = useState(Tokens[0]);
+
+  const [quantity, setQuantity] = useState<number>(0.0);
+
   const { account, library } = useWeb3React<providers.Web3Provider>();
+
+  const data =
+    "0x7f7465737432000000000000000000000000000000000000000000000000000000600057";
   let orderArray;
 
   const fetchOrders = async () => {
     orderArray = await state.p2pDb.orders.toArray();
-    console.log(orderArray[orderArray.length-1]);
+    console.log(orderArray[orderArray.length - 1]);
   };
 
   const matchOrders = async () => {
@@ -26,20 +44,11 @@ function OrderMatch() {
     const signer = library.getSigner();
 
     const address = await signer.getAddress();
-    const data =
-      "0x7f7465737432000000000000000000000000000000000000000000000000000000600057";
 
     // console.log(exchangeAbi)
     // console.log(EXCHANGE)
 
     const exchangeContract = new Contract(EXCHANGE, exchangeAbi, signer);
-
-    const tokenInstance = new Contract(TOKENONE,tokenOneAbi,signer);
-    //read fucntion
-    const tokenBalance = await tokenInstance.balanceOf(address,1);
-    const tx = await tokenInstance.mint(address,1,100,data);
-
-    console.log(`Balance is ${tokenBalance.toString()}`);
 
     console.table(orderOne);
     console.table(orderTwo);
@@ -57,8 +66,8 @@ function OrderMatch() {
     });
 
     await exchangeContract.executeOrder(
-        token2Address.get(orderOne.tokenFrom),
-        token2Address.get(orderTwo.tokenFrom),
+      token2Address.get(orderOne.tokenFrom),
+      token2Address.get(orderTwo.tokenFrom),
       orderOne.orderFrm,
       orderTwo.orderFrm,
       token2Id.get(orderOne.tokenFrom),
@@ -68,6 +77,54 @@ function OrderMatch() {
       data
     );
   };
+
+  const mint = async () => {
+    // @ts-ignore
+    const signer = library.getSigner();
+
+    const address = await signer.getAddress();
+
+    const tokenContractAddress = token2Address.get(token.name);
+
+    const tokenContractAbi = token2Abi.get(token.name);
+
+    const tokenId = token2Id.get(token.name);
+
+    // @ts-ignore
+    const tokenInstance = new Contract(tokenContractAddress,tokenContractAbi,signer);
+    //read fucntion
+    const tx = await tokenInstance.mint(address, tokenId, quantity, data);
+
+    await tx.wait();
+
+    const tokenBalance = await tokenInstance.balanceOf(address, tokenId);
+
+    console.log(`Balance is ${tokenBalance.toString()}`);
+  };
+
+
+  const approval = async() => {
+    // @ts-ignore
+    const signer = library.getSigner();
+
+    const address = await signer.getAddress();
+
+    const tokenContractAddress = token2Address.get(token.name);
+
+    const tokenContractAbi = token2Abi.get(token.name);
+
+    // @ts-ignore
+    const tokenInstance = new Contract(tokenContractAddress,tokenContractAbi,signer);
+
+    //write 
+    const tx = await tokenInstance.setApprovalForAll(EXCHANGE,true);
+
+    await tx.wait();
+
+    const contractBool = await tokenInstance.isApprovedForAll(address,EXCHANGE);
+
+    console.log(contractBool.toString());
+}
 
   return (
     <div className="w-full bg-white border-gray-200 rounded px-4 py-2">
@@ -88,6 +145,45 @@ function OrderMatch() {
         >
           Match Order
         </button>
+
+        <div className="mt-2 mb-6 flex items-center justify-between">
+          <Select
+            label="I Want"
+            range={Tokens}
+            selected={token as SelectObject}
+            setSelected={
+              setToken as React.Dispatch<React.SetStateAction<SelectObject>>
+            }
+          />
+          <div className="w-1/2 mt-1 ml-4 border-b border-gray-300 focus-within:border-orange-600">
+            <input
+              type="number"
+              name="Quantity"
+              id="quantity"
+              step="1"
+              min={0}
+              onChange={(e) => setQuantity(Number(e.target.value))}
+              className="block w-full text-gray-500 border-0 border-b border-transparent bg-gray-50 focus:border-orange-600 focus:ring-0 sm:text-sm"
+              placeholder="0"
+            />
+          </div>
+          <button
+            type="submit"
+            className="mr-0 ml-auto my-4 block flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-orange-600 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
+            onClick={() => mint()}
+          >
+            Mint
+          </button>
+          </div>
+        <div>
+          <button
+            type="submit"
+            className="mr-0 ml-auto my-4 block flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-orange-600 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
+            onClick={() => approval()}
+          >
+            Aprroval
+          </button>
+        </div>
       </div>
     </div>
   );
