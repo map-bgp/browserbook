ae4c75ee5376b344db383d2a6c49c2e8800801f5
tejareddy8888 <saitejapottanigari@gmail.com>
2021-10-11 11:12:50 +0200

    TokenFactory updates

--- client/contracts/Exchange.sol
+++ client/contracts/Exchange.sol
@@ -1,3 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+/**
+ * @title Exchange
+ * @author Teja<saitejapottanigari@gmail.com>, Ankan <ankan0011@live.com>, Corey <corey.bothwell@gmail.com>
+ * @dev Enables the order verification and order matching functionalities.
+ */
+
 pragma solidity 0.8.0;
 
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
@@ -34,4 +42,38 @@ contract Exchange {
             data
         );
     }
+
+    function executeBatchOrder(
+        address tokenOneAddress,
+        address tokenTwoAddress,
+        address tokenOneOwner,
+        address tokenTwoOwner,
+        uint256 tokenOneId,
+        uint256 tokenTwoId,
+        uint256 tokenOneAmount,
+        uint256 tokenTwoAmount,
+        bytes calldata data
+    ) public {
+        // Execute `safeBatchTransferFrom` call
+        // Either succeeds or throws
+        IERC1155(tokenOneAddress).safeTransferFrom(
+            tokenOneOwner,
+            tokenTwoOwner,
+            tokenOneId,
+            tokenOneAmount,
+            data
+        );
+
+        // Execute `safeBatchTransferFrom` call
+        // Either succeeds or throws
+        IERC1155(tokenTwoAddress).safeTransferFrom(
+            tokenTwoOwner,
+            tokenOneOwner,
+            tokenTwoId,
+            tokenTwoAmount,
+            data
+        );
+    }
+
+
 }
--- client/deploy/1_deploy_contracts.js
+++ client/deploy/1_deploy_contracts.js
@@ -6,23 +6,15 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
 
     console.log(`Deployer :${deployer}`);
     // We get the contract to deploy
-    const tx = await deploy("BBookToken", { from: deployer, gasLimit: 12000000, log: true });
+    const tx = await deploy("TokenFactory", { from: deployer, gasLimit: 6000000, log: true });
 
     console.log(tx.transactionHash);
 
-    await ethers.provider.waitForTransaction(tx.transactionHash,2)
-
-    const tx2 = await deploy("BBookToken2", {from: deployer, gasLimit: 12000000, log: true });
-
-    console.log(tx2.transactionHash);
-    
-    await ethers.provider.waitForTransaction(tx2.transactionHash,2)
-
-    const tx3 = await deploy("Exchange", {from: deployer, gasLimit: 12000000, log: true});
+    const tx3 = await deploy("Exchange", {from: deployer, gasLimit: 6000000, log: true});
 
     console.log(tx3.transactionHash);
     
-    await ethers.provider.waitForTransaction(tx3.transactionHash,2)
+    await ethers.provider.waitForTransaction(tx3.transactionHash,1)
 }
 
 module.exports.tags = ['All'];
