53ef15c5c83bfbe58d923084784635555dbff148
Mario <mario@pop-os.localdomain>
2021-12-05 21:25:01 +0100

    the matching algorithm for Market Order

--- client/src/oms/matching.ts
+++ client/src/oms/matching.ts
@@ -1,14 +1,36 @@
 import _ from "lodash";
 
-import { IOrders } from "../db";
-
-export enum tokens {
+export enum Tokens {
   tokenA = "tokenA",
   tokenB = "tokenB",
   tokenC = "tokenC",
 }
+
+export enum OrderType {
+  Market = "Market",
+  Limit = "Limit",
+}
+
+export enum MatchingStatus {
+  Fulfilled = "fulfilled",
+  Started = "Started",
+  Wait = "Wait",
+}
+
+export interface IOrders {
+  id?: string;
+  tokenS: Tokens;
+  tokenB: Tokens;
+  orderType: OrderType;
+  amountS: number;
+  amountB: number;
+  orderFrm: number;
+  from: string;
+  created: string;
+}
+
 export interface tokenSpecificOrder {
-  token: tokens;
+  token: Tokens;
   asks: IOrders[];
   bids: IOrders[];
   countOfAsks: number;
@@ -17,17 +39,39 @@ export interface tokenSpecificOrder {
   pooledLiquidity: number;
 }
 
+export interface MatchingResponse {
+  orderOne: IOrders;
+  orderTwo: IOrders;
+  orderStatus: MatchingStatus;
+  price: number;
+  orderOneQuantity?: number;
+  orderTwoQuantity?: number;
+}
+
+export interface PriceInfo {
+  from: Tokens;
+  to: Tokens;
+  price: number;
+}
 
 export class Matcher {
+  /* What all this matcher contains
+    Step-wise: 
+    1) Firstly it organizes the Orders coming in Tokenwise Order and update in individual using `initialOrderlisting` and `populateLiquidity`
+    2) Secondly, Now I have reduced matchableTokenSetOrders,
+  */
+
   // Tokens Asks and Bids distinguishing helps in matching quicker.
-  //
   public tokenWiseOrders: tokenSpecificOrder[] = [];
+  public matchableTokenSets: tokenSpecificOrder[] = [];
+  public matchableTokens: Tokens[] = [];
+  public totalPooledLiquidity: number;
   constructor(private orderArray: IOrders[]) {}
 
-  initialTokenOrderlisting() {
-    Object.keys(tokens).forEach((key) => {
+  public initialOrderlisting() {
+    Object.keys(Tokens).forEach((key) => {
       this.tokenWiseOrders.push({
-        token: tokens[key],
+        token: Tokens[key],
         asks: [],
         bids: [],
         countOfAsks: 0,
@@ -37,18 +81,19 @@ export class Matcher {
       });
     });
     console.log("Initialized the Asks Bids");
+    return this;
   }
 
-  populateTokenOrders() {
-    for (let key in tokens) {
+  public populateLiquidity() {
+    for (let key in Tokens) {
       const index = this.tokenWiseOrders.findIndex(
-        (x) => x.token === tokens[key]
+        (x) => x.token === Tokens[key]
       );
       const askOrders: IOrders[] = _.filter(this.orderArray, {
-        tokenTo: tokens[key],
+        tokenB: Tokens[key],
       });
       const bidOrders: IOrders[] = _.filter(this.orderArray, {
-        tokenFrom: tokens[key],
+        tokenS: Tokens[key],
       });
       this.tokenWiseOrders[index].asks = askOrders;
       this.tokenWiseOrders[index].bids = bidOrders;
@@ -56,20 +101,161 @@ export class Matcher {
       this.tokenWiseOrders[index].countOfBids = bidOrders.length;
       if (askOrders.length > 0) {
         this.tokenWiseOrders[index].askedLiquidity = askOrders
-          .map((singleOrder) => singleOrder.quantity)
-          .reduce((sum, next) => sum + next);
+          .map((singleOrder) => singleOrder.amountB)
+          .reduce((sum, next) => sum + next, 0);
       }
       if (bidOrders.length > 0) {
         this.tokenWiseOrders[index].pooledLiquidity = bidOrders
-          .map((singleOrder) => singleOrder.quantity * singleOrder.price)
-          .reduce((sum, next) => sum + next);
+          .map((singleOrder) => singleOrder.amountS)
+          .reduce((sum, next) => sum + next, 0);
       }
     }
+    return this;
   }
 
-  Matching() {
-    for (let key in tokens) {
-        
+  processStarted() {
+    const matchingRequest = [];
+    this.matchableTokenSets = _.filter(this.tokenWiseOrders, (o) => {
+      return o.askedLiquidity > 0 && o.pooledLiquidity > 0;
+    });
+    // this.preCleanOrder(matchableTokenSets)
+    this.matchableTokens.push(...this.matchableTokenSets.map((o) => o.token));
+    console.log(this.matchableTokens);
+    console.log(this.matchableTokenSets);
+
+    this.matchableTokenSets.forEach((singleTokenSet) => {
+      singleTokenSet.bids.forEach((order2) => {
+        singleTokenSet.asks.every((order1) => {
+          const matchingResponse = this.orderCompare(order1, order2);
+          if (matchingResponse) {
+            this.handleMatchingResponse(matchingResponse);
+            return false;
+          }
+        });
+      });
+    });
+  }
+
+  handleMatchingResponse(matchingResponse: MatchingResponse) {
+    
+    this.removeOrder(matchingResponse.orderOne);
+    this.removeOrder(matchingResponse.orderTwo);
+  }
+
+  removeOrder(order: IOrders) {
+    this.matchableTokenSets.forEach((element) => {
+      if (element.asks.indexOf(order) !== -1) {
+        element.asks.splice(element.asks.indexOf(order), 1);
+        element.askedLiquidity -= order.amountB;
+      }
+      if (element.bids.indexOf(order) !== -1) {
+        element.bids.splice(element.bids.indexOf(order), 1);
+        element.pooledLiquidity -= order.amountS;
+      }
+    });
+  }
+
+  orderCompare(order1: IOrders, order2: IOrders): MatchingResponse | undefined {
+    // Best CASE 1: Both orders containing same tokens are getting exchanged A<=>B and Same Amount of Tokens are being exchanged.
+    if (
+      order1.tokenS == order2.tokenB &&
+      order1.tokenB == order2.tokenS &&
+      order1.amountB === order2.amountS
+    ) {
+      console.log(`Comparing orders between ${order1.id} and ${order2.id}`);
+
+      const tradeBenefitRatio =
+        (order1.amountS / order1.amountB) * (order2.amountS / order2.amountB);
+
+      if (
+        order1.orderType === OrderType.Market &&
+        order2.orderType === OrderType.Market
+      ) {
+        // CASE1.1: Same tokens at same price
+        if (tradeBenefitRatio == 1 && order1.amountS == order2.amountB) {
+          return {
+            orderOne: order1,
+            orderTwo: order2,
+            orderStatus: MatchingStatus.Fulfilled,
+            price: order1.amountS || order2.amountB,
+          } as MatchingResponse;
+        }
+        // CASE1.2: Same tokens at different quoted price, so mean price of the two quoted
+        else {
+          if (tradeBenefitRatio > 1)
+            return {
+              orderOne: order1,
+              orderTwo: order2,
+              orderStatus: MatchingStatus.Fulfilled,
+              price:
+                order1.amountS < order2.amountB
+                  ? order1.amountS
+                  : order2.amountB,
+            } as MatchingResponse;
+        }
+        return undefined;
+      } else if (
+        order1.orderType === OrderType.Market &&
+        order2.orderType === OrderType.Limit
+      ) {
+        // CASE2.1: Same tokens at same price
+        if (tradeBenefitRatio == 1 && order1.amountS == order2.amountB) {
+          return {
+            orderOne: order1,
+            orderTwo: order2,
+            orderStatus: MatchingStatus.Fulfilled,
+            price: order1.amountS || order2.amountB,
+          } as MatchingResponse;
+        }
+        // CASE2.2: Same tokens at different quoted price, so mean price of the two quoted
+        else if (tradeBenefitRatio > 1 && order1.amountB < order2.amountS) {
+          return {
+            orderOne: order1,
+            orderTwo: order2,
+            orderStatus: MatchingStatus.Fulfilled,
+            price: order1.amountB,
+          } as MatchingResponse;
+        }
+        return undefined;
+      } else if (
+        order1.orderType === OrderType.Limit &&
+        order2.orderType === OrderType.Market
+      ) {
+        // CASE1.1: Same tokens at same price
+        if (tradeBenefitRatio == 1 && order1.amountS == order2.amountB) {
+          return {
+            orderOne: order1,
+            orderTwo: order2,
+            orderStatus: MatchingStatus.Fulfilled,
+            price: order1.amountS || order2.amountB,
+          } as MatchingResponse;
+        }
+        // CASE1.2: Same tokens at different quoted price, so mean price of the two quoted
+        else {
+          if (tradeBenefitRatio > 1 && order1.amountB < order2.amountS)
+            return {
+              orderOne: order1,
+              orderTwo: order2,
+              orderStatus: MatchingStatus.Fulfilled,
+              price: order2.amountS,
+            } as MatchingResponse;
+        }
+        return undefined;
+      } else if (
+        order1.orderType === OrderType.Limit &&
+        order2.orderType === OrderType.Limit
+      ) {
+        // CASE1.1: Same tokens at same price
+        if (tradeBenefitRatio == 1 && order1.amountS == order2.amountB) {
+          return {
+            orderOne: order1,
+            orderTwo: order2,
+            orderStatus: MatchingStatus.Fulfilled,
+            price: order1.amountS || order2.amountB,
+          } as MatchingResponse;
+        }
+        return undefined;
+      }
     }
   }
 }
--- client/src/oms/mock.order.ts
+++ client/src/oms/mock.order.ts
@@ -1,14 +1,12 @@
-import { IOrders } from "../db";
-import { tokens } from "./matching";
+import { IOrders, Tokens, OrderType } from "./matching";
 
 export const OrderA : IOrders = {
     from: "0x12",
-    tokenFrom: tokens.tokenA,
-    tokenTo: tokens.tokenB,
-    orderType: "1",
-    actionType: "2",
-    price: 100,
-    quantity: 10,
+    tokenS: Tokens.tokenA,
+    tokenB: Tokens.tokenB,
+    orderType: OrderType.Market,
+    amountS: 100,
+    amountB: 10,
     orderFrm: 786786,
     created: '1636288913',
     id: "12",
@@ -16,12 +14,11 @@ export const OrderA : IOrders = {
 
 export const OrderB : IOrders = {
     from: "0x13",
-    tokenFrom: tokens.tokenB,
-    tokenTo: tokens.tokenA,
-    orderType: "1",
-    actionType: "2",
-    price: 0.1,
-    quantity: 80,
+    tokenS: Tokens.tokenB,
+    tokenB: Tokens.tokenA,
+    orderType: OrderType.Market,
+    amountS: 10,
+    amountB: 90,
     orderFrm: 23543543,
     created: '1636289212',
     id: "14",
@@ -29,12 +26,11 @@ export const OrderB : IOrders = {
 
 export const OrderC : IOrders = {
     from: "0x14",
-    tokenFrom: tokens.tokenB,
-    tokenTo: tokens.tokenA,
-    orderType: "1",
-    actionType: "2",
-    price: 0.1,
-    quantity: 20,
+    tokenS: Tokens.tokenB,
+    tokenB: Tokens.tokenA,
+    orderType: OrderType.Market,
+    amountS: 1,
+    amountB: 10,
     orderFrm: 23432445,
     created: '1636289367',
     id: "15",
@@ -42,12 +38,11 @@ export const OrderC : IOrders = {
 
 export const OrderD : IOrders = {
     from: "0x15",
-    tokenFrom: tokens.tokenB,
-    tokenTo: tokens.tokenC,
-    orderType: "1",
-    actionType: "2",
-    price: 100,
-    quantity: 20,
+    tokenS: Tokens.tokenB,
+    tokenB: Tokens.tokenC,
+    orderType: OrderType.Market,
+    amountS: 100,
+    amountB: 20,
     orderFrm: 382748394,
     created: '38278932780',
     id: "16",
