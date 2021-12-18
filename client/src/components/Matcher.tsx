6c73c62336760964745964c8471c2d3ae5fdf9e8
tejareddy8888 <saitejapottanigari@gmail.com>
2021-12-18 18:19:52 +0100

    web worker and order matching linking

--- client/src/components/Matcher.tsx
+++ client/src/components/Matcher.tsx
@@ -37,14 +37,14 @@ function Matcher() {
     if (window.Worker) {
       const myWorker = new Worker(new URL('../oms/matching.ts', import.meta.url));
 
-      myWorker.postMessage(10);
+      myWorker.postMessage(orders);
     
       myWorker.onmessage = function(e) {
         console.log(`Message received from worker ${e.data}`);
       }
     } else {
       console.log('Your browser doesn\'t support web workers.');
-    }},1000)
+    }},10000)
   };
 
   /**
@@ -74,6 +74,7 @@ function Matcher() {
     const id = (~~(Math.random() * 1e9)).toString(36) + Date.now();
     await state.p2pDb.validators.add({id: id, peerId: String(state.peerId), joinedTime: Date.now().toString()});
     dispatch(addValidator({peerId: String(state.peerId), address: address, joinedTime: Date.now().toString()}))
+    turnOnMatching();
   };
   
   return (
