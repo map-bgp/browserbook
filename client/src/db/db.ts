7d312b47997fe4121b7e5378e3e827b27d7128d4
blueslurpee <cmbothwell@gmail.com>
2022-01-27 23:49:46 +0100

    added Dexie (wip)

---
+++ client/src/db/db.ts
@@ -0,0 +1,61 @@
+import Dexie from 'dexie'
+
+export interface IPeer {
+  id?: number
+  peerId: string
+  joinedTime: number
+}
+
+export interface IValidator {
+  id?: string
+  peerId: string
+  address: string
+  joinedTime: number
+}
+
+export interface IOrder {
+  id?: string
+  tokenS: string
+  tokenT: string
+  amountS: number
+  amountT: number
+  from: string
+  status: string
+  created: number
+}
+
+export interface IMatchedOrder {
+  id?: string
+  order1: string
+  order2: string
+  tokenS: string
+  tokenT: string
+  amountS: number
+  amountT: number
+  from: number
+  status: string
+  created: number
+}
+
+export class P2PDB extends Dexie {
+  peers: Dexie.Table<IPeer>
+  validators: Dexie.Table<IValidator>
+  orders: Dexie.Table<IOrder>
+  matchedOrders: Dexie.Table<IMatchedOrder>
+
+  constructor() {
+    super('browserbook')
+
+    this.version(1).stores({
+      peers: '++id,peerId',
+      validators: '++id,peerId,address',
+      orders: '++id,tokenS,tokenT,from,status',
+      matchedOrders: '++id,order1,order2,tokenS,tokenT,from,status',
+    })
+
+    this.peers = this.table('peers')
+    this.validators = this.table('validators')
+    this.orders = this.table('orders')
+    this.matchedOrders = this.table('matchedOrder')
+  }
+}
