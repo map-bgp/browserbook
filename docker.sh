538e3bc8f100376a3cff777d524791f276a2c434
Ankan0011 <ankan.ghosh@uzh.ch>
2022-01-31 14:30:12 +0100

    docker build fix and kubernetes CI script.

--- docker.sh
+++ docker.sh
@@ -3,7 +3,7 @@
 echo "Starting docker build"
 
 cd ./client
-docker build . -t browserbook
-docker run -p 8080:5500 -d browserbook
+docker build . -t ankan0011/browserbook
+docker run -p 8080:5500 -d ankan0011/browserbook
 
 echo "Docker Image Created"
