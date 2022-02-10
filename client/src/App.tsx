d3a073e0ec3bb55d45058d5c7c6f9ca90ed7a920
blueslurpee <cmbothwell@gmail.com>
2022-02-11 00:02:43 +0100

    Added animation to let state load

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,8 @@
-import React, { useContext, useEffect } from 'react'
+import React, { Fragment, useContext, useEffect, useState } from 'react'
 import { Location, useLocation } from 'react-router-dom'
 import { withErrorBoundary } from 'react-error-boundary'
+import Lottie from 'lottie-react'
+import blocks from './blocks.json'
 
 import { Navigation } from './components/utils/constants'
 import { getCurrent } from './components/utils/utils'
@@ -8,6 +10,7 @@ import Header from './components/Header'
 import Content from './pages/Content'
 import { AppContext } from './components/AppContext'
 import { useEthers } from './app/Hooks'
+import { Dialog, Transition } from '@headlessui/react'
 
 const AppError = () => (
   <>
@@ -20,6 +23,7 @@ const AppError = () => (
 const App = () => {
   const location: Location = useLocation()
   const { peer } = useContext(AppContext)
+  const [animating, setAnimating] = useState<boolean>(true)
 
   // Have to call this somewhere in the app to initialize ethers connection
   // Could write an initialize hook
@@ -30,14 +34,61 @@ const App = () => {
       await peer.init()
       peer.join()
     }
+
     setup()
   }, [])
 
+  useEffect(() => {
+    setTimeout(() => setAnimating(false), 1000)
+  }, [])
+
   return (
-    <div className="min-h-screen bg-gray-100">
-      <Header current={getCurrent(location, Navigation)} />
-      <Content current={getCurrent(location, Navigation)} />
-    </div>
+    <>
+      <Transition.Root show={animating} as={Fragment}>
+        <Dialog as="div" className="fixed inset-0 z-10 overflow-y-auto" onClose={() => {}}>
+          <div className="flex min-h-screen items-end justify-center px-4 pt-4 pb-20 text-center sm:block sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0 bg-white bg-opacity-100 transition-opacity" />
+            </Transition.Child>
+
+            {/* Tricks the browser to center */}
+            <span className="hidden sm:inline-block sm:h-screen sm:align-middle" aria-hidden="true">
+              &#8203;
+            </span>
+
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <div className="inline-block transform transition-all sm:my-8 sm:w-full sm:max-w-2xl sm:p-6 sm:align-middle">
+                <div className="mx-auto h-64 w-64">
+                  <Lottie animationData={blocks} loop={true} />
+                </div>
+              </div>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition.Root>
+      {!animating && (
+        <div className="min-h-screen bg-gray-100">
+          <Header current={getCurrent(location, Navigation)} />
+          <Content current={getCurrent(location, Navigation)} />
+        </div>
+      )}
+    </>
   )
 }
 
