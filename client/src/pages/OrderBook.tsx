388107eb5e2f4a977065f206c70d035522de5537
blueslurpee <cmbothwell@gmail.com>
2022-02-27 23:49:20 +0100

    Orderbook

--- client/src/pages/OrderBook.tsx
+++ client/src/pages/OrderBook.tsx
@@ -1,18 +1,153 @@
 import { useEffect } from 'react'
 import { useAppDispatch, useAppSelector } from '../app/Hooks'
+import { OrderType } from '../app/p2p/protocol_buffers/gossip_schema'
 import { selectAccountData } from '../app/store/slices/EthersSlice'
-import { getAllOrders } from '../app/store/slices/PeerSlice'
+import { getAllOrders, selectOrdersWithTokenData } from '../app/store/slices/PeerSlice'
+import { getTokens } from '../app/store/slices/TokensSlice'
+import { OrderStatus } from '../app/Types'
+import { classNames } from '../components/utils/utils'
 
 const OrderBook = () => {
   const dispatch = useAppDispatch()
+  const { primaryAccount } = useAppSelector(selectAccountData)
+  const orders = useAppSelector(selectOrdersWithTokenData)
 
   useEffect(() => {
     dispatch(getAllOrders())
   }, [])
 
+  useEffect(() => {
+    if (primaryAccount) {
+      dispatch(getTokens())
+    }
+  }, [primaryAccount])
+
   return (
-    <div className="flex items-center justify-center">
-      <div>You're at the order book chief</div>
+    <div className="mx-auto max-w-7xl py-8 px-8">
+      {/* <OrderModal
+            order={activeOrder}
+            orderToken={activeOrderToken}
+            open={orderModalOpen}
+            setOpen={setOrderModalOpen}
+          /> */}
+      <div className="flex flex-col">
+        <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+            {orders.length !== 0 ? (
+              <div className="overflow-hidden border-b border-gray-200 shadow sm:rounded-lg">
+                <table className="min-w-full divide-y divide-gray-200">
+                  <thead className="bg-gray-50">
+                    <tr>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500"
+                      >
+                        Asset
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500"
+                      >
+                        Type
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500"
+                      >
+                        Price
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500"
+                      >
+                        Limit Price
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500"
+                      >
+                        Quantity
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500"
+                      >
+                        Status
+                      </th>
+                      <th scope="col" className="relative px-6 py-3">
+                        <span className="sr-only">Fill</span>
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {orders.slice(0, 100).map((order, orderIdx) => {
+                      return (
+                        <tr
+                          key={`${order.id}`}
+                          className={orderIdx % 2 === 0 ? 'bg-white' : 'bg-gray-50'}
+                        >
+                          <td
+                            className={classNames(
+                              false ? 'font-medium text-gray-700' : 'text-gray-500',
+                              'whitespace-nowrap px-6 py-4 text-sm ',
+                            )}
+                          >
+                            {order.tokenName}
+                          </td>
+                          <td className="whitespace-nowrap px-6 py-4 text-sm text-gray-500">
+                            {order.orderType === OrderType.BUY ? 'Buy' : 'Sell'}
+                          </td>
+                          <td className="whitespace-nowrap px-6 py-4 text-sm text-gray-500">
+                            Ξ {order.price}
+                          </td>
+                          <td className="whitespace-nowrap px-6 py-4 text-sm text-gray-500">
+                            Ξ {order.limitPrice}
+                          </td>
+                          <td className="whitespace-nowrap px-6 py-4 text-sm text-gray-500">
+                            {order.quantity}
+                          </td>
+                          <td className="whitespace-nowrap px-6 py-4 text-sm text-gray-500">
+                            {order.status === OrderStatus.Matched ? (
+                              <a
+                                title="Order Matched"
+                                className="relative ml-5 inline-flex h-3 w-3 rounded-full bg-green-500"
+                              ></a>
+                            ) : order.status === OrderStatus.Pending ? (
+                              <a
+                                title="Order Pending"
+                                className="relative ml-5 inline-flex h-3 w-3 rounded-full bg-yellow-500"
+                              ></a>
+                            ) : (
+                              <a
+                                title="Order Expired/Rejected"
+                                className="relative ml-5 inline-flex h-3 w-3 rounded-full bg-red-500"
+                              ></a>
+                            )}
+                          </td>
+                          <td className="whitespace-nowrap px-6 py-4 text-right text-sm font-medium">
+                            <div
+                              onClick={() => {
+                                // setActiveOrder(order)
+                                // setActiveOrderToken(!!orderToken ? orderToken : null)
+                                // setOrderModalOpen(true)
+                              }}
+                              className="cursor-pointer text-orange-600 hover:text-orange-900"
+                            >
+                              Fill
+                            </div>
+                          </td>
+                        </tr>
+                      )
+                    })}
+                  </tbody>
+                </table>
+              </div>
+            ) : (
+              <div>It seems you don't have any orders</div>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
